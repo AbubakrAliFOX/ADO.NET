ee43bcd2048a3bb3ba767d86f257adb60c240445
Abubakr <thefox5000@gmail.com>
2024-04-28 17:02:23 +0300

    Parameterized Query With Like

--- Program.cs
+++ Program.cs
@@ -11,6 +11,50 @@ namespace ADO.NET
     {
         static string connetionStr = "server=.;Database=ContactsDB;Integrated Security=True;";
 
+        static void searchContactsStartsWithLetter (string Letter)
+        {
+            SqlConnection connection = new SqlConnection(connetionStr);
+            string query = "select * from Contacts where FirstName LIKE '' + @Letter + '%'";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@Letter", Letter);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int contactID = (int)reader["ContactID"];
+                    string firstName = (string)reader["FirstName"];
+                    string lastName = (string)reader["LastName"];
+                    string email = (string)reader["Email"];
+                    string phone = (string)reader["Phone"];
+                    string address = (string)reader["Address"];
+                    int countryID = (int)reader["CountryID"];
+
+                    Console.WriteLine($"Contact ID: {contactID}");
+                    Console.WriteLine($"Name: {firstName} {lastName}");
+                    Console.WriteLine($"Email: {email}");
+                    Console.WriteLine($"Phone: {phone}");
+                    Console.WriteLine($"Address: {address}");
+                    Console.WriteLine($"Country ID: {countryID}");
+                    Console.WriteLine();
+                }
+
+                reader.Close();
+                connection.Close();
+
+            }
+
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+        }
         static void printAllContactsWithFirstName(string FirstName, int CountryID)
         {
             SqlConnection connection = new SqlConnection(connetionStr);
@@ -103,7 +147,8 @@ namespace ADO.NET
         static void Main(string[] args)
         {
             //printAllContacts();
-            printAllContactsWithFirstName("Jane",2);
+            //printAllContactsWithFirstName("Jane",2);
+            searchContactsStartsWithLetter("m");
 
         }
     }
