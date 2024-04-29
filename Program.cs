1ddfacc7fad931378abee2c7e70b9f2ff219cfc3
Abubakr <thefox5000@gmail.com>
2024-04-29 09:41:41 +0300

    Find contact by ID

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Net;
+using System.Security.Policy;
 
 namespace ADO.NET
 {
@@ -11,6 +13,72 @@ namespace ADO.NET
     {
         static string connetionStr = "server=.;Database=ContactsDB;Integrated Security=True;";
 
+        public struct stContact
+        {
+            public int ID { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Email { get; set; }
+            public string Phone { get; set; }
+            public string Address { get; set; }
+            public int CountryID { get; set; } 
+
+            public void printInfo ()
+            {
+
+                Console.WriteLine($"Contact ID: {ID}");
+                Console.WriteLine($"Name: {FirstName} {LastName}");
+                Console.WriteLine($"Email: {Email}");
+                Console.WriteLine($"Phone: {Phone}");
+                Console.WriteLine($"Address: {Address}");
+                Console.WriteLine($"Country ID: {CountryID}");
+                Console.WriteLine();
+            }
+        }
+        static bool findContactById(int ID, ref stContact contact)
+        {
+            bool isFound = false;
+            SqlConnection connection = new SqlConnection(connetionStr);
+            string query = "select * from Contacts where ContactID = @ID";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@ID", ID);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    isFound = true;
+                    contact.ID = (int)reader["ContactID"];
+                    contact.FirstName = (string)reader["FirstName"];
+                    contact.LastName = (string)reader["LastName"];
+                    contact.Email = (string)reader["Email"];
+                    contact.Phone = (string)reader["Phone"];
+                    contact.Address = (string)reader["Address"];
+                    contact.CountryID = (int)reader["CountryID"];
+
+                } else
+                {
+                    isFound = false;
+                }
+
+                reader.Close();
+                connection.Close();
+
+            }
+
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            return isFound;
+
+        }
         static void searchContactsStartsWithLetter (string Letter)
         {
             SqlConnection connection = new SqlConnection(connetionStr);
@@ -148,7 +216,14 @@ namespace ADO.NET
         {
             //printAllContacts();
             //printAllContactsWithFirstName("Jane",2);
-            searchContactsStartsWithLetter("m");
+            //searchContactsStartsWithLetter("m");
+            stContact contact = new stContact();
+            if (findContactById(1, ref contact)) {
+                contact.printInfo();
+            } else
+            {
+                Console.WriteLine("Not Found!!");
+            }
 
         }
     }
